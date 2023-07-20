708967e1c1a51ca09c128ecd7e4fd12892d84580
fernandadutra <100439986+fernandadutra@users.noreply.github.com>
2023-07-20 15:14:49 -0300

    Add files via upload

---
+++ screens/LogOut.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+export default class LogOut extends Component {
+    render() {
+      return (
+        <View style={styles.container}>
+          <Text>Tela de log out</Text>
+        </View>
+      );
+    }
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      color:"purple",
+    },
+  });
